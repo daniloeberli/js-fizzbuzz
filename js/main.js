9fec89d57eeff943d6b3ec19c5c853f9e37c5e89
daniloeberli <danilo.eberli@gmail.com>
2023-02-23 15:17:21 +0100

    bonus 1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 "use strict"
 
+const ul = document.querySelector("ul.list");
+
+
 for(let i = 1; i <= 100; i++){
+    
+    const li = document.createElement('li')
     if(i%3===0 && i%5===0){
         console.log('FizzBuzz');
+        li.append('FizzBuzz');
+        li.classList.add("fizzbuzz");
     }else if(i%5===0){
         console.log('Buzz');
+        li.append('Buzz');
+        li.classList.add("buzz");
     }else if(i%3===0){
         console.log('Fizz');
+        li.append('Fizz');
+        li.classList.add("fizz");
     }else{
         console.log(i);
+        li.append(i)
     }
+    
+    ul.append(li);
 };
+
+
