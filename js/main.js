8973b75b8359c2795676462a1ddf91039b84bc40
daniloeberli <danilo.eberli@gmail.com>
2023-02-23 16:25:08 +0100

    bonus 2

--- js/main.js
+++ js/main.js
@@ -4,23 +4,24 @@ const ul = document.querySelector("ul.list");
 
 
 for(let i = 1; i <= 100; i++){
-    
+
     const li = document.createElement('li')
     if(i%3===0 && i%5===0){
         console.log('FizzBuzz');
         li.append('FizzBuzz');
-        li.classList.add("fizzbuzz");
+        li.classList.add("fizzbuzz","box");
     }else if(i%5===0){
         console.log('Buzz');
         li.append('Buzz');
-        li.classList.add("buzz");
+        li.classList.add("buzz","box");
     }else if(i%3===0){
         console.log('Fizz');
         li.append('Fizz');
-        li.classList.add("fizz");
+        li.classList.add("fizz","box");
     }else{
         console.log(i);
-        li.append(i)
+        li.append(i);
+        li.classList.add("num","box");
     }
     
     ul.append(li);
